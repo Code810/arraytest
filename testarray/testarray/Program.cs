eb5c67b2416c19ddca7df15c95f1eb5429e72880
code810 <nadirssh@code.edu.az>
2023-10-15 18:07:32 +0400

    first ready

---
+++ testarray/testarray/Program.cs
@@ -0,0 +1,12 @@
+﻿#region testarray
+int[] numbers = { 23, 14, 2, 5, 16 };
+int result = 1;
+//Array.Sort(numbers);
+foreach (var number in numbers)
+{
+    if (number<=20 && number>=1)
+        result*= number;
+    }
+Console.WriteLine(result);
+
+#endregion
